1ede7719bd1d61541bc8c2954d904c21189a0861
Laura McCartney <hello@lauramccartney.co.uk>
2015-08-31 20:42:54 +0100

    Add homepage image

--- wp-content/themes/ivana/home.php
+++ wp-content/themes/ivana/home.php
@@ -6,4 +6,17 @@
 
 get_header(); ?>
 
+<main class="content">
+        
+        <div class="home-bg-image">
+            <p>
+                Lorem ipsum dolor sit amet, nullam numquam dissentiet an nam, nam putant eleifend philosophia ut. Etiam everti iuvaret ex his, ex cum novum primis tractatos. Vivendo molestie delicatissimi nec ex. Deseruisse deterruisset cu usu. Ex vix diam adhuc tibique, et numquam dolorem scripserit mel.
+
+                Te harum accumsan intellegebat qui, esse vero id vis, ius ea dicam officiis praesent. Dicunt detracto pertinax pri an. Ad dicta nullam docendi qui. Mei ignota dicunt complectitur ad. Velit inani comprehensam ad mel, autem iriure liberavisse eu eam.
+            </p>
+        </div>
+        
+        
+</main>
+
 <?php get_footer(); ?>

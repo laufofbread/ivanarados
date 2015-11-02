32ddaf02d21196c33eb082b59d04c58dc7d6368a
Laura McCartney <hello@lauramccartney.co.uk>
2015-11-02 23:20:05 +0000

    Add bigger delay to loading animation

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("#loading-background").transition({
         opacity: 0,
         duration: 1000,
-        delay: 100,
+        delay: 1000,
         complete: function() {
             $(this).remove();
         }
